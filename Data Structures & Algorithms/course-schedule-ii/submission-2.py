d3c3d5be522c00c501f9fc20554b32802ad74a44
matrixSamurai <gatsbyman01@gmail.com>
2026-09-20 17:24:32 -0400

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,53 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+
+        state = [0] * numCourses
+        topo_order = []
+
+        ## Building the adjacency List
+        adj = {i : [] for i in range(numCourses)}
+        for u, v in prerequisites:
+            adj[u].append(v)
+        
+
+        def hasCycle(node):
+
+            # print("The node is")
+            # print(node)
+
+            ## Base cases
+            
+            if state[node] == 1:
+                return True
+            
+            if state[node] == 2:
+                return False
+
+            ## work
+            state[node] = 1
+
+            ## Recursion Calls
+
+            for neighbor in adj[node]:
+                if hasCycle(neighbor):
+                    return True
+            
+            state[node] = 2
+
+            topo_order.append(node)
+
+            # print("The topo order is ")
+            # print(topo_order)
+
+            return False
+
+
+        for i in range(numCourses):
+            if state[i] == 0 and hasCycle(i):
+                return []
+        
+        return topo_order
+
+
+
+        
