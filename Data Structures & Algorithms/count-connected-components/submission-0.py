464dff3ad07ed1394da02a386ff460285f38d155
matrixSamurai <gatsbyman01@gmail.com>
2026-09-24 10:15:50 -0400

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.py
@@ -0,0 +1,47 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+
+        disconnectedComps = n
+
+        parent = [i for i in range(n)]
+
+        rank = [1] * n
+
+
+        def findParent(n):
+
+            if parent[n] == n:
+                return n
+            
+            parent[n] = findParent(parent[n])
+            return parent[n]
+        
+
+        def union(x, y):
+
+            par_x = findParent(x)
+            par_y = findParent(y)
+
+            if par_x == par_y:
+                return
+
+            nonlocal disconnectedComps
+            disconnectedComps -= 1
+
+            if rank[par_x] > rank[par_y]:
+                parent[par_y] = par_x
+            elif rank[par_x] < rank[par_y]:
+                parent[par_x] = par_y
+            else:
+                parent[par_x] = par_y
+                rank[par_y] += 1
+            
+
+        for u, v in edges:
+            union(u,v)
+        
+        return disconnectedComps
+        
+
+
+        
